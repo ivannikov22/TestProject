ad5860327f0d6d002444a163e2d0b67a0e27f0b3
ine <ine@a2club.ru>
2018-04-12 12:48:57 +0300

    test2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Хелло ворлд!");
+            Console.WriteLine("Хелло ворлд!");
             Console.ReadKey();
         }
     }
